6f7f027e51902e0517a20c15aa35259755db556f
maic sebakara <maicseba@gmail.com>
2023-06-14 18:13:02 +0200

    worked on login part and onsubmit function added

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,7 +1,53 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleEmailChange = (e)=>{
+    setEmail(e.target.value);
+    setEmailError();
+  };
+
+  const handlePasswordChange = (e)=>{
+    setPassword(e.target.value);
+    setPasswordError("");
+  }
+
+  const validateInputs = ()=>{
+    let isValid = true;
+
+    if(email === ""){
+      setEmailError("Email is required");
+      isValid = false;
+    }
+
+    if(password === ""){
+      setPasswordError("Password is required");
+      isValid = false;
+    }
+    return isValid;
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(email + " "+password);
+    // if(validateInputs()){
+    //   axios
+    //   .post("", {email,password})
+    //   .then((response)=>{
+    //     console.log(response.data);
+    //   })
+    //   .catch((error)=>{
+    //     console.error(error);
+    //   });
+    // }
+  };
+
   return (
     <>
       <div className="container mx-auto px-4 h-full">
@@ -14,37 +60,10 @@ export default function Login() {
                     Start you Session
                   </h6>
                 </div>
-                {/* <div className="btn-wrapper text-center">
-                  <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    <img
-                      alt="..."
-                      className="w-5 mr-1"
-                      src={require("assets/img/github.svg").default}
-                    />
-                    Github
-                  </button>
-                  <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    <img
-                      alt="..."
-                      className="w-5 mr-1"
-                      src={require("assets/img/google.svg").default}
-                    />
-                    Google
-                  </button>
-                </div> */}
                 <hr className="mt-6 border-b-1 border-blueGray-300" />
               </div>
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                {/* <div className="text-blueGray-400 text-center mb-3 font-bold">
-                  <small>Or sign in with credentials</small>
-                </div> */}
-                <form>
+                <form onSubmit={handleSubmit}>
                   <div className="relative w-full mb-3">
                     <label
                       className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
@@ -53,24 +72,33 @@ export default function Login() {
                       Email
                     </label>
                     <input
-                      type="email"
-                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      type="email" 
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150 "
                       placeholder="Email"
+                      value={email}
+                      onChange={handleEmailChange}
                     />
+                    <label className={`${
+                        emailError.length !== 0 ? "text-red-500" : ""
+                      }`}>{emailError.length!==0 ? emailError : ""}</label>
                   </div>
 
                   <div className="relative w-full mb-3">
                     <label
                       className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
+                      htmlFor="grid-password">
                       Password
                     </label>
                     <input
                       type="password"
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                       placeholder="Password"
+                      value={password}
+                      onChange={handlePasswordChange}
                     />
+                    <label className={`${
+                        passwordError.length !== 0 ? "text-red-500" : ""
+                      }`}>{passwordError.length !==0 ? passwordError : ""}</label>
                   </div>
                   <div>
                     <label className="inline-flex items-center cursor-pointer">
@@ -88,7 +116,7 @@ export default function Login() {
                   <div className="text-center mt-6">
                     <button
                       className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                      type="button"
+                      type="submit" 
                     >
                       Sign In
                     </button>
@@ -100,7 +128,6 @@ export default function Login() {
               <div className="w-1/2">
                 <a
                   href="#pablo"
-                  onClick={(e) => e.preventDefault()}
                   className="text-blueGray-200"
                 >
                   <small>Forgot password?</small>
--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Switch,Route } from "react-router-dom/cjs/react-router-dom.min";
+
+// view
+import Login from "views/auth/Login.js";
 
 export default function Register() {
   return (
@@ -8,41 +12,12 @@ export default function Register() {
           <div className="w-full lg:w-6/12 px-4">
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
               <div className="rounded-t mb-0 px-6 py-6">
-                <div className="text-center mb-3">
-                  <h6 className="text-blueGray-500 text-sm font-bold">
-                    Sign up with
-                  </h6>
-                </div>
                 <div className="btn-wrapper text-center">
-                  <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    <img
-                      alt="..."
-                      className="w-5 mr-1"
-                      src={require("assets/img/github.svg").default}
-                    />
-                    Github
-                  </button>
-                  <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    <img
-                      alt="..."
-                      className="w-5 mr-1"
-                      src={require("assets/img/google.svg").default}
-                    />
-                    Google
-                  </button>
+                  Have an Account
                 </div>
                 <hr className="mt-6 border-b-1 border-blueGray-300" />
               </div>
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                <div className="text-blueGray-400 text-center mb-3 font-bold">
-                  <small>Or sign up with credentials</small>
-                </div>
                 <form>
                   <div className="relative w-full mb-3">
                     <label
@@ -115,6 +90,8 @@ export default function Register() {
                     </button>
                   </div>
                 </form>
+                or <a href="/auth/login" className="underline"> Login</a> In if you have an account 
+                
               </div>
             </div>
           </div>
